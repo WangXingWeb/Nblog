6c4ac0e1d613ac04b97014f55963a3a64d355eca
unknown <wx1620@163.com>
2021-11-15 15:09:09 +0800

    favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,7 +30,7 @@ module.exports = {
     ],
   ],
   head: [
-    ["link", { rel: "icon", href: `/logo.png` }],
+    ["link", { rel: "icon", href: `favicon.ico` }],
     ["link", { rel: "manifest", href: "/manifest.json" }],
     ["meta", { name: "theme-color", content: "#3eaf7c" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
