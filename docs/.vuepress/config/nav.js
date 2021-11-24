f3ad271f5edf9d8bb8783c6a2660744c6fd533a3
星仔 <wx1620@163.com>
2021-11-24 23:50:19 +0800

    add aeticle

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -18,7 +18,9 @@ module.exports = [
         ]
     },
     { text: 'vue', link: '/vue/watch监听器/' },
+    { text: 'react', link: '/react/react笔记/' },
     { text: '工具', link: '/tool/拷贝vscode插件到另一台电脑' },
+    { text: '杂项', link: '/杂项/微信小程序生命周期' },
     { text: '收藏', link: '/收藏/组件库' },
     { text: '旧版', link: 'https://iwangxing.cn/manjuan' }
 ]
